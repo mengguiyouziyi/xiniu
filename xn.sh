a7545db41c582f4660d97a0e0562042b261cf9a7
mengguiyouziyi <775618369@qq.com>
2017-11-02 10:39:08 +0800

    2

--- xn.sh
+++ xn.sh
@@ -4,4 +4,4 @@
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 eval "$(pyenv init -)"
 pyenv activate env354
-nohup python /data1/spider/menggui/xiniu/xiniu.py > /data1/spider/menggui/xiniu/100tiao.out 2>&1 &
+nohup python /data1/spider/menggui/xiniu/xiniu.py >> /data1/spider/menggui/xiniu/100tiao.out 2>&1 &
